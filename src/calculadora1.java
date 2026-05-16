407667fb1ab1c64a4469c651fb5a717d1e11906c
Fabio <fabiohenciso2007@gmail.com>
2026-05-16 15:36:16 -0300

    Sistema de Cadastro de Livros com bd

--- src/calculadora1.java
+++ src/calculadora1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class calculadora {
+public class calculadora1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 

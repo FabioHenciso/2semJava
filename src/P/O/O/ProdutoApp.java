79144b02b6a9ea7b6181c2f48ed6b0de3838ed08
Fabio <fabiohenciso2007@gmail.com>
2026-08-10 12:13:54 -0300

    Update ProdutoApp.java

--- src/P/O/O/ProdutoApp.java
+++ src/P/O/O/ProdutoApp.java
@@ -6,6 +6,25 @@ public class ProdutoApp {
 
         //objetos
 
-        Produto P1 = new Produto (" A ", 10)
+        Produto P1 = new Produto (" A ", 10.0,3);
+        Produto P2 = new Produto (" B ", 20.0, 2);
+        Produto P3 = new Produto ();
+
+        P1.vender();
+        P1.exibirDados();
+        P1.repor();
+        P1.exibirDados();
+
+        P2.vender();
+        P2.exibirDados();
+        P2.repor();
+        P2.exibirDados();
+
+        P3.vender();
+        P3.exibirDados();
+        P3.repor();
+        P3.exibirDados();
+
+        System.out.println("Total de produtos: " + Produto.getTotalProdutos());
     }
 }
